bc6fc70cc1d57c841624d927888763620bfc284d
MikeShah <mikeshah@mail.sfsu.edu>
2020-04-08 11:01:38 -0700

    testing examples

--- scripts/app.js
+++ scripts/app.js
@@ -18,13 +18,20 @@
 
 module.exports = function(robot) {
     //  YOUR CODE HERE
-    robot.hear(/java/i, function(msg) {
-      return msg.send("I only know script");
-    });  
+    const squirrels = ["http://img.skitch.com/20100714-d6q52xajfh4cimxr3888yb77ru.jpg", "https://img.skitch.com/20111026-r2wsngtu4jftwxmsytdke6arwd.png", "http://cl.ly/1i0s1r3t2s2G3P1N3t3M/Screen_Shot_2011-10-27_at_9.36.45_AM.png", "http://shipitsquirrel.github.com/images/squirrel.png"];
     //  Example
      robot.hear(/javascript/i, function(msg) {
        return msg.send("I love writing code!");
      });
+
+    robot.respond(/hi|hello/i, function(msg) {
+      return msg.send("Howdy!");
+    });
+
+    return robot.hear(/ship it/i, function(msg) {
+      return msg.send(msg.random(squirrels));
+    });
+  
   }
   
   /************************************
@@ -41,8 +48,8 @@ module.exports = function(robot) {
   //     return msg.send("Howdy!");
   //   });
   
-  //   /* Random Example
-  //   If a user enters 'ship it' we return a random squirrel, which is popular for symbolizing shipping something with engineers */
+  // //   /* Random Example
+  // //   If a user enters 'ship it' we return a random squirrel, which is popular for symbolizing shipping something with engineers */
   //   return robot.hear(/ship it/i, function(msg) {
   //     return msg.send(msg.random(squirrels));
   //   });
