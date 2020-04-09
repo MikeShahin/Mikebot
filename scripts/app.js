596d01ca4a4a32e27fcfc96f3e9c4cf180c2a73f
MikeShah <mikeshah@mail.sfsu.edu>
2020-04-09 11:33:04 -0700

    updates

--- scripts/app.js
+++ scripts/app.js
@@ -59,26 +59,50 @@ module.exports = function(robot) {
       }
     });
 
+    // robot.respond(/play (.*)/i, function(res) {
+    //   let band;
+    //   band = res.match[1];
+    //   if (band === "crass"||"Crass") {
+    //     return res.reply("https://www.youtube.com/watch?v=xDW3mCQ8qHU");
+    //   } else if (band === "G.I.S.M."||"g.i.s.m"||"GISM"||"gism"||"G.I.S.M") {
+    //     return res.reply("https://www.youtube.com/watch?v=UMvLvsdBzuE");
+    //   } else if (band === "blatz"||"Blatz") {
+    //     return res.reply("https://www.youtube.com/watch?v=tNFwdpWwAQY");
+    //   } else if (band === "The Kinks"||"the Kinks"||"the kinks"||"The kinks"||"kinks"||"Kinks") {
+    //     return res.reply("https://www.youtube.com/watch?v=nnXjxtfKFDM");
+    //   }  else if (band === "Love"||"love") {
+    //     return res.reply("https://www.youtube.com/watch?v=EtYS3EYjVyk");
+    //   } else if (band === "13th Floor Elevators"||"13th floor elevators") {
+    //     return res.reply("https://www.youtube.com/watch?v=0OytJYBfwUk");
+    //   } else if (band === "Hieroglyphics"||"hieroglyphics") {
+    //     return res.reply("https://www.youtube.com/watch?v=kXOUmOFVFPo");
+    //   } else if (band === "Living Legends"||"living legends"||"Living legends"||"living Legends") {
+    //     return res.reply("https://www.youtube.com/watch?v=Fy6iPyWfI3E");
+    //   } else if (band === "Anti-Pop Consortium"||"Anti-pop Consortium"||"anti-pop Consortium"||"anti-pop consortium") {
+    //     return res.reply("https://www.youtube.com/watch?v=90RN42kBwaE");
+    //   }
+    // });
+
     robot.respond(/play (.*)/i, function(res) {
       let band;
       band = res.match[1];
-      if (band === "crass"||"Crass") {
+      if (band === "crass") {
         return res.reply("https://www.youtube.com/watch?v=xDW3mCQ8qHU");
-      } else if (band === "G.I.S.M."||"g.i.s.m"||"GISM"||"gism"||"G.I.S.M") {
+      } else if (band === "G.I.S.M.") {
         return res.reply("https://www.youtube.com/watch?v=UMvLvsdBzuE");
-      } else if (band === "blatz"||"Blatz") {
+      } else if (band === "Blatz") {
         return res.reply("https://www.youtube.com/watch?v=tNFwdpWwAQY");
-      } else if (band === "The Kinks"||"the Kinks"||"the kinks"||"The kinks"||"kinks"||"Kinks") {
+      } else if (band === "The Kinks") {
         return res.reply("https://www.youtube.com/watch?v=nnXjxtfKFDM");
-      }  else if (band === "Love"||"love") {
+      }  else if (band === "Love") {
         return res.reply("https://www.youtube.com/watch?v=EtYS3EYjVyk");
-      } else if (band === "13th Floor Elevators"||"13th floor elevators") {
+      } else if (band === "13th Floor Elevators") {
         return res.reply("https://www.youtube.com/watch?v=0OytJYBfwUk");
-      } else if (band === "Hieroglyphics"||"hieroglyphics") {
+      } else if (band === "Hieroglyphics") {
         return res.reply("https://www.youtube.com/watch?v=kXOUmOFVFPo");
-      } else if (band === "Living Legends"||"living legends"||"Living legends"||"living Legends") {
+      } else if (band === "Living Legends") {
         return res.reply("https://www.youtube.com/watch?v=Fy6iPyWfI3E");
-      } else if (band === "Anti-Pop Consortium"||"Anti-pop Consortium"||"anti-pop Consortium"||"anti-pop consortium") {
+      } else if (band === "Anti-Pop Consortium") {
         return res.reply("https://www.youtube.com/watch?v=90RN42kBwaE");
       }
     });
@@ -94,7 +118,7 @@ module.exports = function(robot) {
     });
 
     robot.hear(/playing soon/i, function(msg) {
-      return msg.send("Check out this wibsite for the latest shows: \n http://www.foopee.com/punk/the-list/");
+      return msg.send("Check out this website for the latest shows: \n http://www.foopee.com/punk/the-list/");
       //return msg.send("Check out this" + <a href='http://www.foopee.com/punk/the-list/'>#{@website}</a> + "for the latest shows");
     });
   
