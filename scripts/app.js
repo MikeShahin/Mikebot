a8071321a39e8ffd45e7901519cfc0b7749dcaf1
MikeShah <mikeshah@mail.sfsu.edu>
2020-04-09 11:42:20 -0700

    updates

--- scripts/app.js
+++ scripts/app.js
@@ -86,13 +86,13 @@ module.exports = function(robot) {
     robot.respond(/play (.*)/i, function(res) {
       let band;
       band = res.match[1];
-      if (band === "crass") {
+      if (band === "Crass") {
         return res.reply("https://www.youtube.com/watch?v=xDW3mCQ8qHU");
       } else if (band === "G.I.S.M.") {
         return res.reply("https://www.youtube.com/watch?v=UMvLvsdBzuE");
       } else if (band === "Blatz") {
         return res.reply("https://www.youtube.com/watch?v=tNFwdpWwAQY");
-      } else if (band === "The Kinks") {
+      } else if (band === "The Kinks" && "kinks") {
         return res.reply("https://www.youtube.com/watch?v=nnXjxtfKFDM");
       }  else if (band === "Love") {
         return res.reply("https://www.youtube.com/watch?v=EtYS3EYjVyk");
@@ -102,7 +102,7 @@ module.exports = function(robot) {
         return res.reply("https://www.youtube.com/watch?v=kXOUmOFVFPo");
       } else if (band === "Living Legends") {
         return res.reply("https://www.youtube.com/watch?v=Fy6iPyWfI3E");
-      } else if (band === "Anti-Pop Consortium") {
+      } else if (band === "Anti-Pop Consortium"||"Anti-pop Consortium"||"anti-pop Consortium"||"anti-pop consortium") {
         return res.reply("https://www.youtube.com/watch?v=90RN42kBwaE");
       }
     });
