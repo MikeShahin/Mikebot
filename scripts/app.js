c36458dbba3bc6f9dc7cface47214e5bb304f5b0
MikeShah <mikeshah@mail.sfsu.edu>
2020-04-10 14:00:51 -0700

    updates

--- scripts/app.js
+++ scripts/app.js
@@ -17,7 +17,6 @@
 // };
 
 module.exports = function(robot) {
-    //  YOUR CODE HERE
 
     robot.hear(/hey|hello/i, function(msg) {
       return msg.send("Howdy! I'm MikeBot, type '@mikebot what can you do?' to see what I'm all about! hey");
@@ -28,25 +27,16 @@ module.exports = function(robot) {
     });
 
     robot.respond(/math/i, function(msg) {
-      return msg.send("I can do basic math functions (bear with me I'm learning more complex algebra as we speak!) type in addition, subtraction, multiplication or division problems and I'll solve them!")
+      return msg.reply("I can do basic math functions type in simple addition, subtraction, multiplication or division problems and I'll solve them!")
     });
 
     robot.respond(/music/i, function(msg) {
-      return msg.send("If you want to recommendations for bands, type @mikebot + what genre you're into + 'bands' (i.e. 'punk bands')(p.s. at the moment I only know punk, rock, or hip hop) and I'll set you up with some good tunes, you can also type 'playing soon' to see what cool shows are coming up in the Bay Area?\n You can also try saying @mikebot play a random song if you want to be surprised")
+      return msg.reply("If you want to recommendations for bands, type @mikebot + what genre you're into + 'bands' (i.e. 'punk bands')(p.s. at the moment I only know punk, rock, or hip hop) and I'll set you up with some good tunes, you can also type 'playing soon' to see what cool shows are coming up in the Bay Area?\n You can also try saying @mikebot play a random song if you want to be surprised")
     });
     
     robot.hear(/playing soon/i, function(msg) {
       return msg.send("Check out this website for the latest shows: \n http://www.foopee.com/punk/the-list/");
-      //return msg.send("Check out this" + <a href='http://www.foopee.com/punk/the-list/'>#{@website}</a> + "for the latest shows");
     });
-    
-    //   robot.http("https://midnight-train")
-    // .header('Accept', 'application/json')
-    // .get() (err, res, body) ->
-    //   # error checking code here
-
-    //   data = JSON.parse body
-    //   res.send "#{data.passenger} taking midnight train going #{data.destination}"
 
     robot.respond(/(.*) bands/i, function(res) {
       let genre;
@@ -81,22 +71,78 @@ module.exports = function(robot) {
         return res.reply("https://www.youtube.com/watch?v=Fy6iPyWfI3E");
       } else if (band === "Anti-Pop Consortium"|| band === "Anti-pop Consortium"|| band === "anti-pop Consortium"|| band === "anti-pop consortium") {
         return res.reply("https://www.youtube.com/watch?v=90RN42kBwaE");
-      }
+      } else return res.reply("Sorry, I don't know that band, please choose from my recommendations")
+    });
+
+    robot.hear(/play anagrams/i, function(msg) {
+      return msg.send("Ok, see how many words you can make with these! (only words with 3 or more letters count)\n reply with submit: and your word\ne a p t");
     });
     
-    // robot.hear(/play (.*)/i, function(msg) {
-    //   let band = msg.match[1];
-  
-    //   switch (band) {
-    //     case 'Crass':
-    //       return msg.reply('https://www.youtube.com/watch?v=90RN42kBwaE');
-    //     case 'G.I.S.M':
-    //       return msg.reply('https://www.youtube.com/watch?v=UMvLvsdBzuE');
+    let score = 0; 
+    robot.hear(/submit: (.*)/i, function(msg) {
+      let word = msg.match[1];
+      if (score > 5) { 
+        return res.reply('You win! No more words left')
+      }
+        switch (word) {
+          case 'pate':
+            score ++
+            return msg.reply("Yipee, I'll add one to your score, currently you're at: " + score);
+          case 'peat':
+            score ++ 
+            return msg.reply("Yipee, I'll add one to your score, currently you're at: " + score);
+          case 'tape':
+            score ++ 
+            return msg.reply("Yipee, I'll add one to your score, currently you're at: " + score);
+          case 'tepa':
+            score ++ 
+            return msg.reply("Yipee, I'll add one to your score, currently you're at: " + score);
+          case 'ape':
+            score ++ 
+            return msg.reply("Yipee, I'll add one to your score, currently you're at: " + score);
+          case 'apt':
+            score ++ 
+            return msg.reply("Yipee, I'll add one to your score, currently you're at: " + score);
+          case 'pat':
+            score ++ 
+            return msg.reply("Yipee, I'll add one to your score, currently you're at: " + score);
+          case 'pea':
+            score ++ 
+            return msg.reply("Yipee, I'll add one to your score, currently you're at: " + score);
+          case 'pet':
+            score ++ 
+            return msg.reply("Yipee, I'll add one to your score, currently you're at: " + score);
+          case 'tap':
+            score ++ 
+            return msg.reply("Yipee, I'll add one to your score, currently you're at: " + score);
+          case 'ate':
+            score ++ 
+            return msg.reply("Yipee, I'll add one to your score, currently you're at: " + score);
+          case 'eat':
+            score ++ 
+            return msg.reply("Yipee, I'll add one to your score, currently you're at: " + score);
+          case 'eta':
+            score ++ 
+            return msg.reply("Yipee, I'll add one to your score, currently you're at: " + score);
+          case 'tae':
+            score ++ 
+            return msg.reply("Yipee, I'll add one to your score, currently you're at: " + score);
+          case 'tea':
+            score ++ 
+            return msg.reply("Yipee, I'll add one to your score, currently you're at: " + score);   
+          // case score === 15:
+          //   score = 0;
+          //   return msg.reply('You win! No more words left');
 
-    //     default:
-    //       return msg.reply(band + 'sucks');
-    //   }
-    // });
+          default:
+            return msg.reply('Sorry, not a word');
+      }
+    });
+
+    robot.hear(/you're at: 1/i, function(msg) {
+      score === 0;
+      return msg.send("You win! No more words left");
+    });
 
     randSong = ['https://www.youtube.com/watch?v=90RN42kBwaE', 'https://www.youtube.com/watch?v=Fy6iPyWfI3E', 'https://www.youtube.com/watch?v=kXOUmOFVFPo', 'https://www.youtube.com/watch?v=0OytJYBfwUk', 'https://www.youtube.com/watch?v=EtYS3EYjVyk', 'https://www.youtube.com/watch?v=nnXjxtfKFDM', 'https://www.youtube.com/watch?v=tNFwdpWwAQY', 'https://www.youtube.com/watch?v=UMvLvsdBzuE', 'https://www.youtube.com/watch?v=xDW3mCQ8qHU'];
     robot.hear(/play a random song/i, function(res) {
